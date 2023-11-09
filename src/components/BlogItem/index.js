b30eb320ee6cd5c2e85566b07663e0d40384db4d
tarun <btaruntej143@gmail.com>
2023-11-09 16:00:04 +0000

    firstcommit

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+import {Link} from 'react-router-dom'
+
+const BlogItem = props => {
+  const {blogItemDetails} = props
+  const {id, imageUrl, topic, title, avatarUrl, author} = blogItemDetails
+  return (
+    <Link to={`/blogs/${id}`} className="blog-item-link ">
+      <div className="item-container">
+        <img className="item-image" src={imageUrl} alt={`item${id}`} />
+        <div className="item-info">
+          <p className="item-topic">{topic}</p>
+          <h1 className="item-title">{title}</h1>
+          <div className="author-info">
+            <img className="avatar" src={avatarUrl} alt={`avatar${id}`} />
+            <p className="author-name">{author}</p>
+          </div>
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default BlogItem
